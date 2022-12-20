505ffd7a45e999fba83065f7fb9ca99366ffb143
Paul Spenke <info@onlineer.de>
2022-12-20 15:00:35 +0800

    update tests

--- src/plugin/__tests__/VueCaly.spec.ts
+++ src/plugin/__tests__/VueCaly.spec.ts
@@ -5,7 +5,7 @@ import VueCaly from '../VueCaly.vue'
 
 describe('VueCaly', () => {
   it('renders properly', () => {
-    const wrapper = mount(VueCaly, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(VueCaly, {  })
+    expect(wrapper.text()).toContain('Mo')
   })
 })
