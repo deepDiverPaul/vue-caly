2b583e499a747739d45bdc0ad9a937a388702333
Paul Spenke <info@onlineer.de>
2022-12-22 11:36:53 +0800

    fix lint

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,19 +1,3 @@
-import type {App, Plugin} from 'vue'
 import VueCaly from "./VueCaly.vue";
-
-// const VueCalyPlugin: Plugin = {
-//     install: (Vue: App) => {
-//         Vue.component("vue-caly", VueCaly);
-//     }
-// };
-//
-// // Automatic installation if Vue has been added to the global scope.
-// // @ts-ignore
-// if (typeof window !== 'undefined' && window.Vue) {
-//     // @ts-ignore
-//     window.Vue.use(VueCaly);
-// }
-//
-// export default VueCalyPlugin;
 
 export {VueCaly}
